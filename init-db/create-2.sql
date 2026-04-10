38bd2922c8367b802b8981ec7c4e35cc3696a33c
Dyndyk0 <Dyndyk0@mail.ru>
2026-04-10 15:55:02 +0700

    tables

--- init-db/create-2.sql
+++ init-db/create-2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE CategoryMetadata (
     id SERIAL PRIMARY KEY,
     typeMetadata_id INT,
-    name VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
     FOREIGN KEY (typeMetadata_id) REFERENCES TypeMetadata(id)
 );
